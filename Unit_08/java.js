34a5e3d69076bf32aeb90c57516c48bfc05437d9
Alex0061 <alex.stefan61@yahoo.com>
2021-11-22 09:52:48 +0000

    Just 1/2

--- Unit_08/java.js
+++ Unit_08/java.js
@@ -1,50 +1,53 @@
 function createNode(element) {
-    return document.createElement(element);
-  }
+  return document.createElement(element);
+}
 
-  function append(parent, el) {
-    return parent.appendChild(el);
-  }
-
-  const numberOfUsers = 12;
-  const allCards = document.getElementById('allAuthorCards');
-  const url = 'https://randomuser.me/api/?results=' + numberOfUsers;
-  fetch(url)
-    .then((resp) => resp.json())
-    .then(function(data) {
-        // console.log(data);
-      let authors = data.results;
-
-      return authors.map(function(author) {
-        let cardText = createNode('p');
-        let cardBody = createNode('div');
-        let cardImage = createNode('img');
-        let card = createNode('div');
-        let cardContainer = createNode('div');
-        let cardTitle = createNode('h5');
+function append(parent, el) {
+  return parent.appendChild(el);
+}
 
 
-        cardText.classList.add("card-text");
+const movieContainer = document.getElementById('movie');
+const url = 'http://www.omdbapi.com/?s=Blade+Runner&apikey=423e4210';
+
+fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data) {
+    console.log(data);
+    var movie = data.Search;
+        var p = createNode('p');
+        var h5 = createNode('h5');
+        var imdbID = createNode('imdbID');
+        var img = createNode('img');
+        var column = createNode('div');
+        var card = createNode('div');
+        var cardBody = createNode('div');
+        var a = createNode('a');
         cardBody.classList.add("card-body");
-        cardTitle.classList.add("card-title");
-        cardImage.classList.add("card-img-top");
-        card.classList.add("card")
-        cardContainer.classList.add("p-3");
-        cardContainer.classList.add("col-md-2");
+        card.classList.add("card");
+        card.classList.add("mt-3");
+        a.classList.add("btn");
+        a.classList.add("btn-primary");
+        a.innerHTML = "About This Movie";
+        a.href = "java2.html";
+        column.classList.add("col-3");
+        h5.innerHTML = `${movie.Title}`;
+        h5.classList.add("card-title")
+        p.innerHTML = `${movie.Year}`;
+        p.classList.add("card-text")
+        imdbID.innerHTML = `${movie.imdbID}`;
+        img.src = movie.Poster;
+        img.classList.add("card-img-top");
 
-
-        cardImage.src = author.picture.large;
-        cardTitle.innerHTML = `${author.name.first} ${author.name.last}`;
-        cardText.innerHTML = `${author.location.city}, ${author.phone}`;
-
-        append(cardBody, cardTitle);
-        append(cardBody, cardText);
-        append(card, cardImage);
+        append(card, img);
+        append(cardBody, h5);
+        append(cardBody, p);
+        append(cardBody, a);
         append(card, cardBody);
-        append(cardContainer, card);
-        append(allCards, cardContainer);
-      })
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+        append(column, card);
+        append(movieContainer, column);
+
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
